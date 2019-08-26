f9f0d02d2ccf83d1050c41fb850ed880edebea1c
Francesco Malagrino <francesco.malagrino@gmail.com>
2019-08-26 18:45:32 +0100

    Cisco Project
    Cisco Project

---
+++ README.md
@@ -0,0 +1,18 @@
+Frontend developer coding challenge.
+
+Create a React application that displays a page with a list of Doctor Who episodes.
+Use Maze TV API to fetch all the episodes:
+http://api.tvmaze.com/search/shows?q=doctor%20who&embed=episodes
+
+1. Page should display cards for all episodes grouped by season
+2. Each card should display an episode name, number and season
+3. Each card should link to a detailed episode page which contains episode summary 
+4. User should be able to search for episodes with a keyword match in their summary
+
+You can use Create React App or any boilerplate repository available to start the app, but be prepared to explain your decision. Feel free to use any approach for fetching, storing and querying data.
+
+Extra points for:
+- unit tests
+- development/production webpack configuration
+
+
---
+++ src/containers/SingleSeries/index.js
@@ -0,0 +1,142 @@
+import React ,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+
+const styles = {
+    appBar: {
+        position: 'relative',
+    },
+    flex: {
+        flex: 1,
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+};
+
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
+class SingleSeries extends Component{
+    state={
+        open:false,
+        show:null
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    componentDidMount(){
+        const {id} = this.props.match.params;
+
+        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+            .then((response)=> response.json())
+            .then(json => this.setState({show:json,open:true}))
+    }
+    render(){
+        const {show} =this.state;
+        return(
+            <div >
+                { show !== null &&
+                    <Dialog
+                        fullScreen
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        TransitionComponent={Transition}
+                    >
+                        <AppBar style={styles.appBar}>
+                            <Toolbar>
+                                <Link to={'/'}>
+                                    <IconButton color="default" onClick={this.handleClose} aria-label="Close">
+                                        <CloseIcon />
+                                    </IconButton>
+                                </Link>
+                                <Typography variant="title" color="inherit" style={styles.flex}>
+                                    {show.name}
+                                </Typography>
+                            </Toolbar>
+                        </AppBar>
+
+                        <div style={{
+                            flex: 1,
+                            display:'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <div>
+                                <ListItem button>
+                                    {
+                                        show.image != null &&
+                                        <img alt="Show" src={show.image.medium}/>
+                                    }
+                                </ListItem>
+                            </div>
+
+                            <div>
+                                <List>
+                                 <ListItem button>
+                                        <ListItemText primary="Summary" secondary={show.summary.replace(/<\/?[a-z]+>/gi, '')} />
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem button>
+                                        <ListItemText primary="Name First episode" secondary={show._embedded.episodes[0].name} />
+                                    </ListItem>
+                                    <Divider />
+                                     <ListItem button>
+                                        <ListItemText primary="Name Last episode" secondary={show._embedded.episodes[show._embedded.episodes.length-1].name} />
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem button>
+                                        <ListItemText primary="Season" secondary={show._embedded.episodes[0].season}/>
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem button>
+                                        <ListItemText primary="Last Season" secondary={show._embedded.episodes[show._embedded.episodes.length-1].season} />
+                                    </ListItem>
+                                    <Divider />
+                                      <ListItem button>
+                                        <ListItemText primary="Number" secondary={show._embedded.episodes[0].number} />
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem button>
+                                        <ListItemText primary="Last Number episode" secondary={show._embedded.episodes[show._embedded.episodes.length-1].number} />
+                                    </ListItem>
+                                    <Divider />
+                                     <ListItem button>
+                                        <ListItemText primary="Url first episode" secondary={show._embedded.episodes[0].url} />
+                                    </ListItem>
+                                    <Divider />
+                                     <ListItem button>
+                                        <ListItemText primary="Url last episode" secondary={show._embedded.episodes[show._embedded.episodes.length-1].url} />
+                                    </ListItem>
+                                    <Divider />
+                                    <ListItem button>
+                                        <ListItemText primary="Total Episodes" secondary={show._embedded.episodes.length} />
+                                    </ListItem>
+                                    <Divider />
+                                   
+                                </List>
+                            </div>
+                        </div>
+                    </Dialog>
+            }
+            </div>
+        )
+    }
+
+}
+
+export default SingleSeries;
